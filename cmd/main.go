d3e19905a47bedb7ccda71e561620cfc65147730
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2024-01-24 10:40:39 +0100

    load

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-    "github.doublematt.tutorialcheck/TutorialCheck/pkg/books"
-    "github.doublematt.tutorialcheck/TutorialCheck/pkg/common/db"
-    "github.com/gin-gonic/gin"
-    "github.com/spf13/viper"
+	"github.com/Doublematt/TutorialCheck/pkg/books"
+	"github.com/Doublematt/TutorialCheck/pkg/common/db"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
-    viper.SetConfigFile("./pkg/common/envs/.env")
-    viper.ReadInConfig()
+	viper.SetConfigFile("./pkg/common/envs/.env")
+	viper.ReadInConfig()
 
-    port := viper.Get("PORT").(string)
-    dbUrl := viper.Get("DB_URL").(string)
+	port := viper.Get("PORT").(string)
+	dbUrl := viper.Get("DB_URL").(string)
 
-    router := gin.Default()
-    dbHandler := db.Init(dbUrl)
+	router := gin.Default()
+	dbHandler := db.Init(dbUrl)
 
-    books.RegisterRoutes(router, dbHandler)
+	books.RegisterRoutes(router, dbHandler)
 
-    router.GET("/", func(ctx *gin.Context) {
-        ctx.JSON(200, gin.H{
-            "port":  port,
-            "dbUrl": dbUrl,
-        })
-    })
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"port":  port,
+			"dbUrl": dbUrl,
+		})
+	})
 
-    router.Run(port)
+	router.Run(port)
 }
